2edcc080744929ea17939d60f5c0609bfc089cf9
ltlaitoff <ltlaitoff@gmail.com>
2023-05-07 18:28:57 +0300

    tests(Dummy): Created `Dummy` spec for check gh actions

---
+++ src/Dummy.spec.tsx
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest';
+
+describe('Dummy spec', () => {
+  it('true to be true', () => {
+    expect(true).toBe(true);
+  });
+
+  it('false to be false', () => {
+    expect(false).toBe(false);
+  });
+});
