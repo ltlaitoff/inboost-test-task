04b4b1376c6eb0b1b5ed0a469f2b99c8f95b6d39
ltlaitoff <ltlaitoff@gmail.com>
2023-05-08 20:17:54 +0300

    feat(App): Using `store.context` in App. StoreProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import Header from './containers/Header/Header.component';
 import Sidebar from './containers/Sidebar/Sidebar.component';
 import Workspace from './containers/Workspace/Workspace.component';
+import { StoreProvider } from './contexts/store.context';
 
 function App() {
   return (
-    <>
+    <StoreProvider>
       <Header />
 
       <div className="flex">
         <Sidebar />
         <Workspace />
       </div>
-    </>
+    </StoreProvider>
   );
 }
 
