8646687f175c593ee78168eb0a1aed4bf626c795
ltlaitoff <ltlaitoff@gmail.com>
2023-05-08 20:18:37 +0300

    feat(SearchBox): Changed `useState` to `useStore` store.context

--- src/containers/Header/components/SearchBox/SearchBox.component.tsx
+++ src/containers/Header/components/SearchBox/SearchBox.component.tsx
@@ -1,10 +1,12 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent } from 'react';
+import { useStore } from '../../../../contexts/store.context';
+import { setSearchAction } from '../../../../store/store.actions';
 
 function SearchBox() {
-  const [searchValue, setSearchValue] = useState('');
+  const { state, dispatch } = useStore();
 
   function onChange(e: ChangeEvent<HTMLInputElement>) {
-    setSearchValue(e.target.value);
+    dispatch(setSearchAction(e.target.value));
   }
 
   return (
@@ -12,7 +14,7 @@ function SearchBox() {
       <input
         className="h-full px-3 text-slate-600 rounded-md hover:border-2 hover:border-purple-400"
         placeholder="Search"
-        value={searchValue}
+        value={state.search}
         onChange={onChange}
       />
     </div>
