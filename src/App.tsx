8a213e31e8fd7af747982d09536ee18387a6ed98
ltlaitoff <ltlaitoff@gmail.com>
2023-05-11 02:54:32 +0300

    refactor(main, App): Moved `StoreProvider` from `App` to `main.ts`

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import Header from './containers/Header/Header.component'
 import Sidebar from './containers/Sidebar/Sidebar.component'
 import Workspace from './containers/Workspace/Workspace.component'
-import { StoreProvider } from './contexts/store.context'
+import { useStore } from './contexts/store.context'
+import { database } from './utils/database'
+import { setStateAction } from './store/store.actions'
 
 function App() {
 	return (
-		<StoreProvider>
-			<div className="h-full flex flex-col">
-				<Header />
+		<div className="h-full flex flex-col">
+			<Header />
 
-				<div className="w-full max-w-[1440px] mx-auto flex h-full">
-					<Sidebar />
-					<Workspace />
-				</div>
+			<div className="w-full max-w-[1440px] mx-auto flex h-full">
+				<Sidebar />
+				<Workspace />
 			</div>
+		</div>
 		</StoreProvider>
 	)
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { StoreProvider } from './contexts/store.context.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<StoreProvider>
+			<App />
+		</StoreProvider>
 	</React.StrictMode>
 )
