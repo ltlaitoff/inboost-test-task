7e605e9a894bad2761c38aecff9a940de8dc7934
ltlaitoff <ltlaitoff@gmail.com>
2023-05-10 16:59:01 +0300

    feat(Workspace): Edit is works!

--- src/containers/Workspace/Workspace.component.tsx
+++ src/containers/Workspace/Workspace.component.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { ChangeEvent, useMemo } from 'react'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
@@ -6,9 +6,10 @@ import './Workspace.component.css'
 
 import { useStore } from '../../contexts/store.context'
 import { transformDateForOutput } from './helpers'
+import { updateNoteAction } from '../../store/store.actions'
 
 function Workspace() {
-	const { state } = useStore()
+	const { state, dispatch } = useStore()
 
 	const currentNote = useMemo(() => {
 		return state.notes.find(note => note.id === state.chosenNoteId)
@@ -16,18 +17,41 @@ function Workspace() {
 
 	if (!currentNote) return null
 
+	const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+		if (!currentNote) return
+
+		dispatch(
+			updateNoteAction({
+				...currentNote,
+				content: e.target.value
+			})
+		)
+	}
+
 	return (
-		<main className="w-full">
+		<main className="w-full h-full flex flex-col">
 			<div className="mt-2 text-sm text-center text-slate-400">
 				{transformDateForOutput(currentNote.date)}
 			</div>
 
-			<ReactMarkdown
-				className="mt-3 markdown-body px-4 py-2"
-				remarkPlugins={[remarkGfm]}
-			>
-				{currentNote.content}
-			</ReactMarkdown>
+			{state.chosenNoteStatus === 'preview' && (
+				<ReactMarkdown
+					className="markdown-body mt-3 px-4 py-2 h-full overflow-y-scroll"
+					remarkPlugins={[remarkGfm]}
+				>
+					{currentNote.content}
+				</ReactMarkdown>
+			)}
+
+			{state.chosenNoteStatus === 'edit' && (
+				<div className="mt-3 px-4 py-2 h-full">
+					<textarea
+						className="w-full h-full"
+						onChange={e => onChange(e)}
+						defaultValue={currentNote.content}
+					></textarea>
+				</div>
+			)}
 		</main>
 	)
 }
