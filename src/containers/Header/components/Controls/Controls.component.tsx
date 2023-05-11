e3f9d01889bdceb369d0a83f8acafef018caed21
ltlaitoff <ltlaitoff@gmail.com>
2023-05-12 00:36:49 +0300

    feat(Controls): Using

--- src/containers/Header/components/Controls/Controls.component.tsx
+++ src/containers/Header/components/Controls/Controls.component.tsx
@@ -1,10 +1,12 @@
 import ControlDelete from '../ControlDelete/ControlDelete.component'
 import ControlAdd from '../ControlAdd/ControlAdd.component'
 import ControlEdit from '../ControlEdit/ControlEdit.component'
+import ControlToggleSidebar from '../ControlToggleSidebar/ControlToggleSidebar.component'
 
 function Controls() {
 	return (
 		<div className="flex gap-x-1 py-1">
+			<ControlToggleSidebar />
 			<ControlAdd />
 			<ControlDelete />
 			<ControlEdit />
