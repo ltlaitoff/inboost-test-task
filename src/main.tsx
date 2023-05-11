b16fa0beb05ca4620e2e4fe647318348cbddfa31
ltlaitoff <ltlaitoff@gmail.com>
2023-05-11 03:30:17 +0300

    feat(main): Removed `StrictMode` React

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { StoreProvider } from './contexts/store.context.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
-		<StoreProvider>
-			<App />
-		</StoreProvider>
-	</React.StrictMode>
+	<StoreProvider>
+		<App />
+	</StoreProvider>
 )
